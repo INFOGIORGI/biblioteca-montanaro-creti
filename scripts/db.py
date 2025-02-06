3d432c665de6f83434553bf76249a1d6e979fcfe
Christian Montanaro <117461585+Chriyl@users.noreply.github.com>
2025-02-06 10:44:31 +0100

    Update db.py

--- scripts/db.py
+++ scripts/db.py
@@ -1,8 +1,8 @@
 """
 
-CREATE DATABASE Biblio2;
+CREATE DATABASE Biblioteca;
 
-USE Biblio2;
+USE Biblioteca;
 
 CREATE TABLE IF NOT EXISTS Tessere(
         idTessera CHAR(39),
@@ -49,13 +49,21 @@ CREATE TABLE IF NOT EXISTS Produzioni(
 
 CREATE TABLE IF NOT EXISTS Prestiti(
         id INT AUTO_INCREMENT PRIMARY KEY,
-        idLibro INT,
-        idTessera CHAR(39),
+        idLibro INT NOT NULL,
+        idTessera CHAR(39) NOT NULL,
         dataInizio DATE NOT NULL,
         dataFine DATE NOT NULL,
         restituto BOOLEAN default False NOT NULL,
         FOREIGN KEY (idLibro) REFERENCES Libri(id) ON DELETE CASCADE,
         FOREIGN KEY (idTessera) REFERENCES Tessere(idTessera) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS Catalogo(
+        idLibro INT NOT NULL,
+        sezione VARCHAR(255) NOT NULL,
+        indice INT NOT NULL,
+        disponibile BOOLEAN NOT NULL ,
+        FOREIGN KEY (idLibro) REFERENCES Libri(id) ON DELETE CASCADE
 );
 
 """
