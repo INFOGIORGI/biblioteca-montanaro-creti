bc0d7baf646a6b7ef6c0346b129b6be8a10fb79b
Christian <Christianmontanaro390@gmail.com>
2025-02-07 16:00:30 +0100

    added login and register root

--- app.py
+++ app.py
@@ -5,16 +5,24 @@ app = Flask(__name__)
 users = ['Alice', 'Bob', 'Charlie'] 
 
 @app.route("/")
-def hello():
+def hello() -> str:
     return render_template("index.html", message='Ciao mondo!!')
 
 @app.route("/users")
-def user():
+def user() -> str:
     return render_template('users.html', users=users)
 
-@app.route("/user/<utente>")
-def utente(utente):
+@app.route("/user/<utente>") 
+def utente(utente) -> str:
     return render_template('profile.html', utente=utente)
 
+@app.route("/register" ,methods=["GET", "POST"])
+def register() -> str:
+    return render_template("register.html")
+
+    
+@app.route("/login", methods=["GET", "POST"])
+def register() -> str:
+    return render_template("login.html")
 
 app.run(debug=True)
