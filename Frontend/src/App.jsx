9175c645fcea0a9b3a369f8f6c641d5468c7de2a
Christian <Christianmontanaro390@gmail.com>
2025-02-19 15:47:21 +0100

    add router

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Importiamo gli elementi necessari
+
 import LibriForm from './pages/libriForm'
 
 function App() {
 
 
   return (
-    <LibriForm />
+    <Router>
+      <Routes>
+        <Route path='/aggiungiLibri' element={<LibriForm />}/>
+      </Routes> 
+    </Router>
   )
 }
 
