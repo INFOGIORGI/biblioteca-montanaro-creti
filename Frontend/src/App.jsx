91e7bee49a16fb98d9624290dcee750c712d0fe7
Christian <Christianmontanaro390@gmail.com>
2025-02-19 19:23:34 +0100

    added navbar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,13 +2,16 @@ import './App.css'
 import axios from 'axios';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Importiamo gli elementi necessari
 import LibriForm from './pages/libriForm'
+import Navbar from './assets/components/navbar';
 axios.defaults.baseURL = "http://127.0.0.1:5000"
 
 function App() {
 
 
   return (
+    
     <Router>
+      <Navbar />
       <Routes>
         <Route path='/aggiungiLibri' element={<LibriForm />}/>
       </Routes> 
---
+++ Frontend/src/assets/components/navbar.jsx
@@ -0,0 +1,89 @@
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="bg-gray-500 text-white shadow-lg">
+      <div className="cnt mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <div className="flex-shrink-0 mr-4">
+             
+            </div>
+            <div className="hidden md:block">
+              <ul className="flex justify-start gap-10 h-16 font-dinpro font-bold items-center">
+                <li>
+                  <NavLink
+                    className={({ isActive }) =>
+                      isActive ? 'text-black' : ''
+                    }
+                    end
+                    to="/aggiungiLibri"
+                  >
+                    Home
+                  </NavLink>
+                </li>
+                
+              </ul>
+            </div>
+          </div>
+          <div className="md:hidden">
+            <button
+              onClick={toggleMenu}
+              className="text-gray-300 hover:text-white focus:outline-none focus:text-white"
+            >
+              <svg
+                className="h-6 w-6"
+                stroke="currentColor"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                {isOpen ? (
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M18.364 17.364a1 1 0 0 1-1.414 0L12 12.414l-4.95 4.95a1 1 0 0 1-1.414-1.414L10.586 11 5.636 6.05a1 1 0 0 1 1.414-1.414L12 9.586l4.95-4.95a1 1 0 0 1 1.414 1.414L13.414 11l4.95 4.95a1 1 0 0 1 0 1.414z"
+                  />
+                ) : (
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M4 5h16M4 12h16m-7 7h7"
+                  />
+                )}
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {isOpen && (
+        <div className="md:hidden">
+          <ul className="flex flex-col items-center gap-4 p-4 font-dinpro font-bold  ">
+            <li>
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? 'text-black' : ''
+                }
+                end
+                to="/aggiungiLibri"
+                onClick={toggleMenu}
+              >
+                Home
+              </NavLink>
+            </li>
+           
+          </ul>
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
