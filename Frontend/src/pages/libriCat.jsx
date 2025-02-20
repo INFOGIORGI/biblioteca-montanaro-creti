5a7269c162021536bde9bff9fddc8f0f5a0fc084
Christian <Christianmontanaro390@gmail.com>
2025-02-21 00:44:01 +0100

    update every 10 sec

--- Frontend/src/pages/libriCat.jsx
+++ Frontend/src/pages/libriCat.jsx
@@ -5,10 +5,11 @@ import axios from "axios"
 import { useEffect, useState } from "react"
 
 
+
 const LibriCat = () => {
     const [libri, setLibri] = useState([])
 
-    useEffect(() => {
+    const prendiLibri = () => {
         axios.get("api/Libri")
         .then((response) => {
             setLibri(response.data)
@@ -18,16 +19,31 @@ const LibriCat = () => {
         .catch((errore) => {
             console.log(errore)
         })
+    }
+
+    useEffect(() => {
+        prendiLibri()
+
+        const timer = setInterval(prendiLibri, 10000);
+        return () => clearInterval(timer)
     }, [])
 
     return (<>
-       <div className="grid mx-10 mt-5 grid-cols-3 gap-4">
-            {libri.map((value) => (
-            <div key={value.id} className="p-4 border rounded-lg shadow">
-                <Libro titolo={value.titolo} isbn={value.ISBN} data={value.dataPub} />
+
+            {libri.length === 0 ?(
+                <p className="text-center text-gray-500 text-2xl mt-5">Nessun Libro disponibile :(</p>
+            ) : (<div className="grid mx-10 mt-5 grid-cols-3 gap-4">
+
+            
+            
+                {libri.map((value) => (
+                <div key={value.id} className="p-4 border rounded-lg shadow">
+                    <Libro titolo={value.titolo} isbn={value.ISBN} data={value.dataPub} />
+                </div>
+                ))}
             </div>
-            ))}
-        </div>
+        )}
+       
     </>)
 }
 
