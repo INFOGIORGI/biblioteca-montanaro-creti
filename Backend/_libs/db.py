b4adea186ae6735b50fd33f29f2d176614130cab
Christian <Christianmontanaro390@gmail.com>
2025-02-26 23:21:55 +0100

    scrittura su file completata

--- Backend/_libs/db.py
+++ Backend/_libs/db.py
@@ -2,11 +2,16 @@ from flask_mysqldb import MySQL
 import os
 from typing import TYPE_CHECKING
 import re
+import threading
+import time
+import json
+from datetime import date, datetime
 
 
 class Database:
     def __init__(self, app ):
         
+        #db var
         self.app = app
         self.app.config['MYSQL_HOST'] = "138.41.20.102"
         self.app.config['MYSQL_USER'] = "5di"
@@ -19,13 +24,19 @@ class Database:
         conn = self.mysql.connection
         cursor = conn.cursor()
         cursor.execute("SHOW TABLES")
-        tables = cursor.fetchall()
+        self.tables = cursor.fetchall()
         cursor.close()
 
         
 
+      
+
+    
+
+        
+
         # Creazione dinamica dei modelli per ogni tabella
-        for (table_name,) in tables:
+        for (table_name,) in self.tables:
             print(f"tabella trovata: {table_name}")
 
             model = BaseModel(table_name, self)
@@ -33,6 +44,87 @@ class Database:
                 setattr(model, "getLibriConAutori", self.getLibriConAutori.__get__(model)) # setto un metodo custom solo all'istanza con nome "Libri" quella get mi serve per richiamare l'istanza correttamente
 
             setattr(self, table_name, model) 
+        
+        #thread var
+        self.running = False
+        self.thread = None
+        self.startService()
+    
+
+
+    def converti_dati(self, dati, column_names):
+        """
+        praticamente converti valore mi controlla se quel valore é un istanza di date o datetime 
+        e se lo é me la converte in un formato testuale leggibile con la notazione ISO
+        obv gli passo tutta la tupla e solo se trova un valore con il formata data agisce e solo
+        su quel valore
+
+        -christian 
+        """
+        def converti_valore(val):
+            if isinstance(val, (date, datetime)):
+                return val.isoformat()
+            return val
+        return [{column: converti_valore(value) for column, value in zip(column_names, row)} for row in dati]
+
+    def scriviFile(self):
+        """
+        questa funzione é semi complessa ma una volta capita passa la paura
+        in sostanza mi prendo le istanze create dal db a runtime model = getattr()
+        mi prendo il nome delle tabelle e i dati e creo un json
+        il tutto viene eseguito da un thread parallelo ogni 5 min
+        
+        praticamente ho convertito un mariaDB in un mongoDB
+
+        -christian
+        """
+        while self.running:
+            with self.app.app_context():
+                try:
+                    if not os.path.exists("dbjson"):
+                        os.makedirs("dbjson")
+
+                    for (table_name,) in self.tables:
+                        model = getattr(self, table_name, None) # mi prendo l'istanza dell'oggeto corrente del db
+                        if model is None:
+                            print(f"⚠️ Errore: {table_name} non trovato nel Database!") # giustamente se non torna niente none esiste la tabella, puó succedere
+                            continue
+
+                        dati = model.getAll() 
+                        if not dati:
+                            print(f"Nessun dato trovato per la tabella {table_name}") # stessa cosa ma con i valori
+                            continue
+
+                        conn = self.getConn()
+                        cursor = conn.cursor()
+                        cursor.execute(f"SELECT * FROM {table_name} LIMIT 1") # faccio una query che mi torna una sola tupla per non sprecare risorse perché mi serve solo per il nomme delle colonne
+                        column_names = [desc[0] for desc in cursor.description] # me li prendo medianete cursor.description
+                        cursor.close()
+
+                        dati_json = self.converti_dati(dati, column_names)
+
+                        file_path = f"dbjson/{table_name}.json"
+                        with open(file_path, "w", encoding="utf-8") as f:
+                            json.dump(dati_json, f, indent=4, ensure_ascii=False)
+
+                        print(f"✅ File {file_path} aggiornato con successo!")
+                except Exception as e:
+                    print(f"❌ Errore durante la scrittura dei file JSON: {e}")
+            time.sleep(300)
+
+
+    def startService(self):
+        if not self.running:
+            self.running = True
+            self.thread = threading.Thread(target=self.scriviFile, daemon=True)
+            self.thread.start()
+            print(f"thread avviato a {self.thread}")
+    
+    def stopService(self):
+        if self.thread:
+            self.thread.join()
+            print(f"{self.thread} terminato")
+
 
     def getLibriConAutori(self):
         _conn = self.db.getConn()
